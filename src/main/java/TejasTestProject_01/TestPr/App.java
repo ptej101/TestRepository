b8f2af09bd5a59efe47557fdd3de9c01e1c379a3
TEJAS <TEJAS@Lenovo-PC>
2022-08-24 18:50:21 +0530

    FirstCommitGitCheck01

---
+++ src/main/java/TejasTestProject_01/TestPr/App.java
@@ -0,0 +1,13 @@
+package TejasTestProject_01.TestPr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
